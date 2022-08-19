a374d5f725d77301a0f9699fa2d0eb822f3a5eff
pletko <paytonjletko@gmail.com>
2022-08-19 08:22:08 -0500

    Added scroll to top
    Added scroll to top

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,37 @@ function menuToggle() {
   }
 
 }
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+//When user scrolls down 20px from the top of the document, show the button
+window.onscroll = function()
+{scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20
+    ||
+  document.documentElement.scrollTop > 20) {
+    mybutton.style.display =
+    "block";
+  }
+  else {
+    mybutton.style.display =
+    "none";
+  }
+}
+
+//When user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; //for Safari
+  document.documentElement.scrollTop = 0; //for Chrome, Firefox, IE, and Opera
+}
+
+//Not working yet 
+function scrollFunction() {
+  mybutton.scrollTo({
+    top:0,
+    behavior: "smooth"
+  })
+}

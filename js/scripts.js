7232b6e12e1907156fc6124ca413608d3dc7c33e
pletko <paytonjletko@gmail.com>
2022-08-29 18:47:46 -0500

    color updates
    accessibility color updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,29 @@
-console.log("Hi! Welcome to My Portfolio Site");
+console.log('Hi! Welcome to My Portfolio Site')
 
 function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+  var x = document.getElementById('myNavtoggle')
 
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
-  }
-
-}
-
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-//When user scrolls down 20px from the top of the document, show the button
-window.onscroll = function()
-{scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20
-    ||
-  document.documentElement.scrollTop > 20) {
-    mybutton.style.display =
-    "block";
-  }
-  else {
-    mybutton.style.display =
-    "none";
+    x.className = 'navtoggle'
   }
 }
 
-//When user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; //for Safari
-  document.documentElement.scrollTop = 0; //for Chrome, Firefox, IE, and Opera
+// Get the button
+mybutton = document.getElementById('myBtn')
+
+// When user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction ()
+function scrollFunction () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) { mybutton.style.display = 'block' }
+  else { mybutton.style.display =
+    'none'
+  }
+}
+
+// When user clicks on the button, scroll to the top of the document
+function topFunction () {
+  document.body.scrollTop = 0 // for Safari
+  document.documentElement.scrollTop = 0 // for Chrome, Firefox, IE, and Opera
 }
